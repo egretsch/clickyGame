bb2107f5d29bd2b296736d043af54aa4466af02a
Edward <edwardgretsch@gmail.com>
2018-09-19 10:14:02 -0700

    update8

--- clickygame/src/components/Navbar/Navbar.js
+++ clickygame/src/components/Navbar/Navbar.js
@@ -2,10 +2,10 @@ import React from "react";
 import "./Navbar.css";
 
 const Navbar = props => (
-    <div>
-        <ul className="nav nav-pills nav-justified">
-            <li><a href="/">Anime Clicky Game</a></li>
-            <li
+    <div  >
+        <ul  className="nav nav-pills nav-justified">
+            <li className="floatl"><a href="/">Anime Clicky Game</a></li>
+            <li style={{ position: "absolute", marginLeft: "25%", width: "50%"}}
                 className={props.message.indexOf('incorrectly') !== -1 ?
                     "desc-incorrect" :
                     props.message.indexOf('correctly') !== -1 ?
@@ -14,7 +14,7 @@ const Navbar = props => (
             >
                 {props.message}
             </li>
-            <li>Score: <span style={{ color: "yellow" }}>{props.curScore}</span> | Top Score: {props.topScore}</li>
+            <li style={{ position: "absolute", right: 5}}>Score: <span style={{  color: "yellow" }}>{props.curScore}</span> | Top Score: {props.topScore}</li>
         </ul>
     </div>
 );
